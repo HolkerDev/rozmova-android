0fb2bfd2c357456357f4504625ac01badb2bb072
HolkerDev <mykola.penkovskyi@tooltime.de>
2025-02-17 15:10:20 +0100

    translation: add translation

--- app/src/main/java/eu/rozmova/app/components/StopChatButton.kt
+++ app/src/main/java/eu/rozmova/app/components/StopChatButton.kt
@@ -6,6 +6,8 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import eu.rozmova.app.R
 
 @Composable
 fun StopChatButton(
@@ -17,6 +19,6 @@ fun StopChatButton(
         modifier = modifier.fillMaxSize(),
         shape = MaterialTheme.shapes.medium,
     ) {
-        Text("Finish discussion")
+        Text(stringResource(R.string.finish_discussion))
     }
 }
