7094675e8ac4ca4de36735220dc17aab3399b643
HolkerDev <mykola.penkovskyi@tooltime.de>
2025-02-17 21:31:09 +0100

    feat: make lang change support older versions

--- app/src/main/java/eu/rozmova/app/utils/LocaleManager.kt
+++ app/src/main/java/eu/rozmova/app/utils/LocaleManager.kt
@@ -1,5 +1,6 @@
 package eu.rozmova.app.utils
 
+import android.app.Activity
 import android.app.LocaleManager
 import android.content.Context
 import android.os.Build
@@ -19,14 +20,41 @@ class LocaleManager
             if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
                 context.getSystemService(LocaleManager::class.java)?.applicationLocales =
                     LocaleList(Locale(languageCode))
+            } else {
+                updateResources(context, languageCode)
             }
         }
 
         fun getCurrentLocale(): Locale =
             if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
-                context.getSystemService(LocaleManager::class.java)?.applicationLocales?.get(0)
+                context.getSystemService(LocaleManager::class.java)?.getApplicationLocales()?.get(0)
                     ?: Locale.getDefault()
             } else {
-                Locale.getDefault()
+                context.resources.configuration.locales
+                    .get(0)
             }
+
+        @Suppress("DEPRECATION")
+        private fun updateResources(
+            context: Context,
+            language: String,
+        ) {
+            val locale = Locale(language)
+            Locale.setDefault(locale)
+
+            val resources = context.resources
+            val configuration = resources.configuration
+
+            configuration.setLocale(locale)
+            configuration.setLayoutDirection(locale)
+
+            val displayMetrics = resources.displayMetrics
+            @Suppress("DEPRECATION")
+            resources.updateConfiguration(configuration, displayMetrics)
+
+            // Recreate the activity to apply changes
+            if (context is Activity) {
+                context.recreate()
+            }
+        }
     }
