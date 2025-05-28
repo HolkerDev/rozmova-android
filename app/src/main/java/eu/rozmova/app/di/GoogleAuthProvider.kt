b8cd97a5f61e4394291d0086435a7ef6ffc53d25
HolkerDev <mykola.penkovskyi@tooltime.de>
2025-05-28 17:49:31 +0200

    feat: remove unused btn

--- app/src/main/java/eu/rozmova/app/di/GoogleAuthProvider.kt
+++ app/src/main/java/eu/rozmova/app/di/GoogleAuthProvider.kt
@@ -11,32 +11,38 @@ import dagger.hilt.android.qualifiers.ApplicationContext
 import eu.rozmova.app.BuildConfig
 import javax.inject.Inject
 
-class GoogleAuthProvider @Inject constructor(
-    private val credentialManager: CredentialManager,
-    @ApplicationContext private val context: Context
-) {
-    suspend fun getGoogleCredential(hashedNonce: String): Result<String> {
-        return try {
-            val googleIdOption = GetGoogleIdOption.Builder()
-                .setFilterByAuthorizedAccounts(false)
-                .setServerClientId(BuildConfig.GOOGLE_CLIENT_ID)
-                .setNonce(hashedNonce)
-                .build()
+class GoogleAuthProvider
+    @Inject
+    constructor(
+        private val credentialManager: CredentialManager,
+        @ApplicationContext private val context: Context,
+    ) {
+        suspend fun getGoogleCredential(hashedNonce: String): Result<String> =
+            try {
+                println(BuildConfig.GOOGLE_CLIENT_ID)
+                val googleIdOption =
+                    GetGoogleIdOption
+                        .Builder()
+                        .setFilterByAuthorizedAccounts(false)
+                        .setServerClientId(BuildConfig.GOOGLE_CLIENT_ID)
+                        .setNonce(hashedNonce)
+                        .build()
 
-            val request = GetCredentialRequest.Builder()
-                .addCredentialOption(googleIdOption)
-                .build()
+                val request =
+                    GetCredentialRequest
+                        .Builder()
+                        .addCredentialOption(googleIdOption)
+                        .build()
 
-            val result = credentialManager.getCredential(context, request)
-            val googleIdTokenCredential = GoogleIdTokenCredential.createFrom(result.credential.data)
+                val result = credentialManager.getCredential(context, request)
+                val googleIdTokenCredential = GoogleIdTokenCredential.createFrom(result.credential.data)
 
-            Result.success(googleIdTokenCredential.idToken)
-        } catch (e: GetCredentialException) {
-            Result.failure(e)
-        } catch (e: GoogleIdTokenParsingException) {
-            Result.failure(e)
-        } catch (e: Exception) {
-            Result.failure(e)
-        }
+                Result.success(googleIdTokenCredential.idToken)
+            } catch (e: GetCredentialException) {
+                Result.failure(e)
+            } catch (e: GoogleIdTokenParsingException) {
+                Result.failure(e)
+            } catch (e: Exception) {
+                Result.failure(e)
+            }
     }
-}
--- app/src/main/java/eu/rozmova/app/screens/chats/ChatsListScreen.kt
+++ app/src/main/java/eu/rozmova/app/screens/chats/ChatsListScreen.kt
@@ -6,14 +6,10 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Add
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.FloatingActionButton
-import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
@@ -98,21 +94,6 @@ fun ChatsListScreen(
                     }
                 }
             }
-        }
-
-        FloatingActionButton(
-            onClick = onChatCreateClick,
-            modifier =
-                Modifier
-                    .padding(16.dp)
-                    .align(Alignment.BottomEnd),
-            containerColor = MaterialTheme.colorScheme.tertiary,
-            contentColor = MaterialTheme.colorScheme.onTertiary,
-        ) {
-            Icon(
-                imageVector = Icons.Default.Add,
-                contentDescription = stringResource(R.string.create_chat_content_description),
-            )
         }
     }
 }
