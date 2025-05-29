976e87751c9a9b19d6e2986816bd5e57da761af5
HolkerDev <mykola.penkovskyi@tooltime.de>
2025-05-29 21:51:57 +0200

    Bump version to 0.0.8 (8)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -32,8 +32,8 @@ android {
         applicationId = "eu.rozmova.app"
         minSdk = 24
         targetSdk = 35
-        versionCode = 7
-        versionName = "0.0.7"
+        versionCode = 8
+        versionName = "0.0.8"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
