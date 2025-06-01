6893c10789d782732a45eadde7bf1c5c8fe0ef7c
HolkerDev <mykola.penkovskyi@tooltime.de>
2025-06-01 20:01:47 +0200

    Bump version to 0.0.11 (11)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -32,8 +32,8 @@ android {
         applicationId = "eu.rozmova.app"
         minSdk = 24
         targetSdk = 35
-        versionCode = 10
-        versionName = "0.0.10"
+        versionCode = 11
+        versionName = "0.0.11"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
