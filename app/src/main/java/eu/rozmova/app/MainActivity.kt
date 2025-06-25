368f5119933a4a3dcbad1af1de3cf7b86b0027f1
HolkerDev <mykola.penkovskyi@tooltime.de>
2025-06-25 22:56:27 +0200

    fix: top bar indentation problem

--- app/src/main/java/eu/rozmova/app/MainActivity.kt
+++ app/src/main/java/eu/rozmova/app/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.statusBars
@@ -18,6 +19,7 @@ import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.core.view.WindowCompat
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
@@ -167,7 +169,7 @@ private fun App(viewModel: AppViewModel = hiltViewModel()) {
                     BottomNavBar(route, navController)
                 }
             },
-            contentWindowInsets = WindowInsets.statusBars,
+            contentWindowInsets = WindowInsets(0),
             modifier = Modifier.fillMaxSize(),
         ) { innerPadding ->
             NavigationHost(navController, innerPadding)
@@ -245,6 +247,9 @@ private fun getCurrentRoute(navController: NavHostController): String? {
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        enableEdgeToEdge()
+        WindowCompat.setDecorFitsSystemWindows(window, false)
 
         setContent {
             App()
