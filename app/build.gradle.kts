055eab2f1bc01a691f7ab32683b5ec9c24dcaa29
HolkerDev <mykola.penkovskyi@tooltime.de>
2025-06-26 18:27:04 +0200

    Bump version to 0.0.24 (24)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -32,8 +32,8 @@ android {
         applicationId = "eu.rozmova.app"
         minSdk = 24
         targetSdk = 35
-        versionCode = 23
-        versionName = "0.0.23"
+        versionCode = 24
+        versionName = "0.0.24"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
