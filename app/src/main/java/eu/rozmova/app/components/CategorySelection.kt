6f45b94f2399cf04103c741c783799ff574b0305
HolkerDev <mykola.penkovskyi@tooltime.de>
2025-07-20 16:19:27 +0200

    fix: minor padding fix

--- app/src/main/java/eu/rozmova/app/components/CategorySelection.kt
+++ app/src/main/java/eu/rozmova/app/components/CategorySelection.kt
@@ -32,7 +32,6 @@ import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -82,7 +81,7 @@ fun RecentlyAdded(
         modifier =
             modifier
                 .fillMaxWidth()
-                .padding(horizontal = 8.dp, vertical = 8.dp),
+                .padding(horizontal = 8.dp, vertical = 4.dp),
         elevation = CardDefaults.cardElevation(defaultElevation = 2.dp),
         colors =
             CardDefaults.cardColors(
