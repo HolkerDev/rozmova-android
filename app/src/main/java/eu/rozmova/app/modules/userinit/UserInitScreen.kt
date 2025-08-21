d2602bc0b454817146ebd413e76099d823c66021
HolkerDev <mykola.penkovskyi@tooltime.de>
2025-08-21 22:34:59 +0200

    feat: add user init screen

---
+++ app/src/main/java/eu/rozmova/app/modules/userinit/UserInitScreen.kt
@@ -0,0 +1,92 @@
+package eu.rozmova.app.modules.userinit
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.hilt.navigation.compose.hiltViewModel
+import eu.rozmova.app.domain.Level
+import org.orbitmvi.orbit.compose.collectAsState
+
+@Composable
+fun UserInitScreen(
+    hobbies: Set<String>,
+    job: String?,
+    pronoun: String,
+    level: Level,
+    toLearn: () -> Unit,
+    modifier: Modifier = Modifier,
+    viewModel: UserInitVM = hiltViewModel(),
+) {
+    val state by viewModel.collectAsState()
+
+    LaunchedEffect(viewModel) {
+        viewModel.initUser(hobbies, job, pronoun, level)
+    }
+
+    Content(
+        isReady = state.isReady,
+        onStartLearning = {},
+        modifier = modifier,
+    )
+}
+
+@Composable
+private fun Content(
+    isReady: Boolean,
+    onStartLearning: () -> Unit,
+    modifier: Modifier = Modifier,
+) {
+    Scaffold(modifier = modifier) { paddingValues ->
+        Column(
+            modifier =
+                Modifier
+                    .padding(paddingValues)
+                    .fillMaxSize(),
+        ) {
+            Box(
+                modifier =
+                    Modifier
+                        .weight(1f)
+                        .fillMaxWidth(),
+                contentAlignment = Alignment.Center,
+            ) {
+                if (!isReady) {
+                    CircularProgressIndicator()
+                }
+            }
+            Button(
+                onClick = onStartLearning,
+                enabled = isReady,
+                modifier =
+                    Modifier
+                        .fillMaxWidth()
+                        .padding(8.dp),
+                shape = MaterialTheme.shapes.medium,
+            ) {
+                Text("Start Learning")
+            }
+        }
+    }
+}
+
+@Preview
+@Composable
+private fun UserInitScreenPreview() {
+    MaterialTheme {
+        Content(isReady = false, onStartLearning = {})
+    }
+}
---
+++ app/src/main/java/eu/rozmova/app/modules/userinit/UserInitVM.kt
@@ -0,0 +1,74 @@
+package eu.rozmova.app.modules.userinit
+
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import eu.rozmova.app.domain.Level
+import eu.rozmova.app.services.UserInitData
+import eu.rozmova.app.services.UserInitState.CreatingBucket
+import eu.rozmova.app.services.UserInitState.Error
+import eu.rozmova.app.services.UserInitState.Finished
+import eu.rozmova.app.services.UserInitState.Idle
+import eu.rozmova.app.services.UserInitState.SavingData
+import eu.rozmova.app.services.UserService
+import org.orbitmvi.orbit.Container
+import org.orbitmvi.orbit.ContainerHost
+import org.orbitmvi.orbit.viewmodel.container
+import javax.inject.Inject
+
+data class UserInitState(
+    val isReady: Boolean = false,
+    val error: Boolean = false,
+)
+
+@HiltViewModel
+class UserInitVM
+    @Inject
+    constructor(
+        private val userService: UserService,
+    ) : ViewModel(),
+        ContainerHost<UserInitState, Unit> {
+        override val container: Container<UserInitState, Unit> = container(UserInitState())
+
+        init {
+            subscribeToUserInitProgress()
+        }
+
+        private fun subscribeToUserInitProgress() =
+            intent {
+                userService.userInitProgress.collect { events ->
+                    when (events) {
+                        CreatingBucket -> {
+                            reduce { state.copy(isReady = false) }
+                        }
+                        Error -> {
+                            reduce { state.copy(error = true) }
+                        }
+                        Finished -> {
+                            reduce { state.copy(isReady = false) }
+                        }
+                        Idle -> {
+                            reduce { state.copy(isReady = false) }
+                        }
+                        SavingData -> {
+                            reduce { state.copy(isReady = false) }
+                        }
+                    }
+                }
+            }
+
+        fun initUser(
+            hobbies: Set<String>,
+            job: String?,
+            pronoun: String,
+            level: Level,
+        ) = intent {
+            userService.startUserInit(
+                UserInitData(
+                    job = job,
+                    hobbies = hobbies.toList(),
+                    pronoun = pronoun,
+                    level = level,
+                ),
+            )
+        }
+    }
